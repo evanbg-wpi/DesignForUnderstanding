5ac3b12bd1c143ec3e21b74f2da4dfcc9ab4a675
Evan Goldstein <evan@icarusnet.me>
2018-03-29 12:41:24 -0400

    Table

--- table.js
+++ table.js
@@ -12,12 +12,7 @@ import * as p5 from "./p5/p5";
 var csv;
 
 function preload() {
-    //my table is comma separated value "csv"
-    //and has a header specifying the columns labels
     table = loadTable('earthquakes.csv', 'csv', 'header');
-    //the file can be remote
-    //table = loadTable("http://p5js.org/reference/assets/mammals.csv",
-    //                  "csv", "header");
 }
 
 function setup() {
