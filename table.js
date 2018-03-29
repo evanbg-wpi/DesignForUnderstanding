fa6abc7e97550a6a8a8651dd49b29dd7ded118d8
Evan Goldstein <evan@icarusnet.me>
2018-03-29 12:38:33 -0400

    Table

--- table.js
+++ table.js
@@ -6,31 +6,37 @@
 // 1,Panthera pardus,Leopard
 // 2,Equus zebra,Zebra
 
-var table;
+
+import * as p5 from "./p5/p5";
+
+var csv;
 
 function preload() {
     //my table is comma separated value "csv"
     //and has a header specifying the columns labels
-    //table = loadTable('earthquake', 'csv', 'header');
+    table = loadTable('earthquakes.csv', 'csv', 'header');
     //the file can be remote
     //table = loadTable("http://p5js.org/reference/assets/mammals.csv",
     //                  "csv", "header");
 }
 
 function setup() {
-//     //count the columns
-//     print(table.getRowCount() + ' total rows in table');
-//     print(table.getColumnCount() + ' total columns in table');
+    let table = new p5.Table();
+    table.addRow(new p5.TableRow;
+    //count the columns
+    print( + ' total rows in table');
+    print(csv.getColumnCount() + ' total columns in table');
 
-//     print(table.getColumn('name'));
-//     //["Goat", "Leopard", "Zebra"]
+    print(csv.getColumn('name'));
+    //["Goat", "Leopard", "Zebra"]
 
-//     //cycle through the table
-//     for (var r = 0; r < table.getRowCount(); r++)
-//         for (var c = 0; c < table.getColumnCount(); c++) {
-//             print(table.getString(r, c));
-//         }
- }
+    //cycle through the table
+    for (var r = 0; r < table.getRowCount(); r++)
+        for (var c = 0; c < table.getColumnCount(); c++) {
+            print(table.getString(r, c));
+        }
+}
+
 function draw(){
     ellipse(50,50,80,80);
 }
