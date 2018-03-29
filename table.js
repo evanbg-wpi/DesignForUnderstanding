ebbef6b628a0a83edd0f9e645a050ff8694e18e6
Alazar Genene <alazar.genene@gmail.com>
2018-03-29 12:38:01 -0400

    table.js added

--- table.js
+++ table.js
@@ -9,28 +9,24 @@
 var table;
 
 function preload() {
-    //my table is comma separated value "csv"
-    //and has a header specifying the columns labels
-    //table = loadTable('earthquake', 'csv', 'header');
+    // my table is comma separated value "csv"
+    // and has a header specifying the columns labels
+    table = loadTable('earthquakes.csv', 'csv', 'header');
     //the file can be remote
-    //table = loadTable("http://p5js.org/reference/assets/mammals.csv",
-    //                  "csv", "header");
+    //table = loadTable("http://p5js.org/reference/assets/earthquake.csv", "csv", "header");
 }
 
 function setup() {
-//     //count the columns
-//     print(table.getRowCount() + ' total rows in table');
-//     print(table.getColumnCount() + ' total columns in table');
+    //count the columns
+    print(table.getRowCount() + ' total rows in table');
+    print(table.getColumnCount() + ' total columns in table');
 
-//     print(table.getColumn('name'));
-//     //["Goat", "Leopard", "Zebra"]
+    print(table.getColumn('name'));
+    //["Goat", "Leopard", "Zebra"]
 
-//     //cycle through the table
-//     for (var r = 0; r < table.getRowCount(); r++)
-//         for (var c = 0; c < table.getColumnCount(); c++) {
-//             print(table.getString(r, c));
-//         }
+    //cycle through the table
+    for (var r = 0; r < table.getRowCount(); r++)
+        for (var c = 0; c < table.getColumnCount(); c++) {
+            print(table.getString(r, c));
+        }
  }
-function draw(){
-    ellipse(50,50,80,80);
-}
