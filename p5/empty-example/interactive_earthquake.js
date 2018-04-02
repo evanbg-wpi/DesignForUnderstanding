d788239b9c9436bf9ca18010b652a0624ec33743
msgonsalves <msgonsalves@wpi.edu>
2018-04-01 22:27:48 -0400

    moved earthquake.csv to its own folder to upload to a server

--- p5/empty-example/interactive_earthquake.js
+++ p5/empty-example/interactive_earthquake.js
@@ -1,51 +1,85 @@
-import * as mappa from "../addons/mappa";
+
+var csv;
+const options = {
+    lat: 0,
+    lng: 0,
+    zoom: 1,
+    style: "http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+}
+
+var mappa = new Mappa('Leaflet');
+
+var myMap;
+var canvas;
+
+function preload() {
+    csv = loadTable('http://127.0.0.1:8080/earthquakes.csv', 'csv', 'header');
+}
 
 function setup() {
-    //import image of map
 
+    canvas = createCanvas(500,520);
+
+    myMap = mappa.tileMap(options);
+    myMap.overlay(canvas);
+
+    myMap.onChange(draw);
 }
 
-function getEarthquakes(map){
-   //get dimmensions of  map
 
-    var listsOfEarthquakes = get2DArray(100);
+
+function getEarthquakes(){
+
+    var LOE = {};
 
     //pull all earthquakes from database
-   //itterate through earthquakes and place them in lists
-    var anEarthquake = {lon: 0, lat: 0, magnitude: 0};
-        //get one earthquake
-        //convert cordinates to flat earth model
 
+    let result = [];
 
-    console.log(source + "This is the source");
+    let obj = {};
 
-        //convert this to pixels on map
-        //place earthquake in appropriate group
-        //var correct element = getInnerArray(lon, lat);
+    for (let i = 1; i < csv.getRowCount(); i++) {
 
-    return listsOfEarthquakes;
+        tempLat = Number(csv.getString(i, 'latitude'));
+        tempLon = Number(csv.getString(i, 'longitude'));
 
+        const pos = myMap.latLngToPixel(tempLat, tempLon);
+
+        //let groupNum = getInnerArray(pos.x, pos.y);
+
+        obj[headers[latitude]] = tempLat;
+        obj[headers[longitude]] = tempLon;
+        obj[headers[magnitude]] = Number(csv.getString(i, 'magnitude'));
+
+        LOE.push(obj);
+        //LOE[groupNum].push(obj);
+
+    }
+    return LOE;
 }
 
-function getInnerArray(lon, lat, map){
+function getInnerArray(lat, lon){
 
-    width = map.options.width / 10;
-    height = map.options.height /10;
+    width = myMap.options.width / 10;
+    height = myMap.options.height /10;
 
 
-    var firstDigit = lon/width * 10
-    var secondDigit = lat/height
+    var firstDigit = (lat/width).floor * 10;
+    var secondDigit = (lon/height).floor;
 
-    //var placeInArray = firstDigit + secondDigit
+    var placeInArray = firstDigit + secondDigit;
 
-    //return placeInArray;
+    return placeInArray;
 }
 
 function draw(){
 
     //get image of map
-    var map = mappa.StaticMap();
-    var listOfEarthquakes = getEarthquakes(map);
+    var LOE = getEarthquakes();
+
+    for( var i = 0; i < csv.getRowCount(); i++){
+        ellipse(LOE.latitude, LOE.longitude, LOE.magnitude*100, LOE.magnitude*100);
+    }
 
 
 
